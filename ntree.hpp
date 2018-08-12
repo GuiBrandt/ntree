98b20a027832f6d3278c3ef5c48ec91fcaad6c9c
GuiBrandt <gui.g.brandt@gmail.com>
2018-08-12 00:22:22 -0300

    Comentários :^)

--- ntree.hpp
+++ ntree.hpp
@@ -3,28 +3,51 @@
 
 #include <iostream>
 
-/*
-    Implementação de uma árvore n-ária genérica
-
-    @author     Guilherme Guidotti Brandt
-    @version    0.1.1
-    @date       2018-08-12
-*/
+/**
+ * @brief Árvore n-ária de busca
+ * 
+ * @tparam N Número de nós filhos por nó
+ * @tparam T Tipo de valor armazenado na árvore
+ */
 template <unsigned int N, class T> class n_tree
 {
     private:
-        T* info[N - 1];
-        int last_index;
+        T* info[N - 1];         //! Vetor de informações
+        int last_index;         //! Última posição ocupada no vetor de informações
 
-        n_tree* branches[N];
+        n_tree* branches[N];    //! Vetor de nós filhos
 
     public:
+        /**
+         * @brief Construtor
+         */
         n_tree();
+        /**
+         * @brief Destrutor
+         */
         ~n_tree();
-
+        /**
+         * @brief Determina se a árvore está vazia
+         * 
+         * @return true se a árvore não tiver nenhum elemento ou nó filho
+         * @return false 
+         */
         bool empty();
-        void insert(T);
-
+        /**
+         * @brief Insere uma informação na árvore
+         * 
+         * @param data Dados a serem inseridos na árvore
+         */
+        void insert(const T& data);
+        /**
+         * @brief Escreve uma árvore para uma stream de saída em ordem
+         * 
+         * @param out Stream de saída
+         * @param tree Árvore a ser escrita
+         * @return std::ostream& A stream recebida por parâmetro
+         */ 
+        // Nota: Isso precisa estar dentro da declaração da classe devido ao 
+        // parâmetro genérico (n_tree<N, T>) em uma "função amiga"
         friend std::ostream& operator<<(std::ostream& out, const n_tree<N, T>& tree)
         {
             for (int i = 0; i <= tree.last_index; i++)
